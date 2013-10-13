631d1aeff7d8f040589b66d38fe07edb0275d361
Emre Hasegeli <hasegeli@tart.com.tr>
2013-10-13 14:49:08 +0300

    Fix timeout message.

--- libtart/database.py
+++ libtart/database.py
@@ -16,6 +16,7 @@
 
 import signal
 import fcntl
+from datetime import datetime
 
 class Timeout (Exception): pass
 
@@ -23,36 +24,37 @@ class TimestampDatabase:
     '''Database to read and write only one timestamp in ISO format. Allow single user by block the file when used.
     Set SIGALRM to enter the database. Initilize the database implicitly with current timestamp on first read.'''
 
+    def __init__(self, filename):
+        self.__filename = filename
+        signal.signal(signal.SIGALRM, self.__timeoutRaiser)
+
     __enterTimeoutSeconds = 10
     __openTimeoutSeconds = 300
 
-    def __timeoutRaiser(self, *arguments):
-        if not self.__databaseLocked:
-            raise Timeout('Database could not locked.')
-        raise Timeout
-
-    def __init__(self, filename):
-        self.__filename = filename
-        self.__databaseLocked = False
-        signal.signal(signal.SIGALRM, self.__timeoutRaiser)
-        self.__databaseLocked = False
-
     def __enter__(self):
         self.__pointer = open(self.__filename, 'a+')
+        self.__databaseLocked = False
         signal.alarm(self.__enterTimeoutSeconds)
-        self.__databaseLocked = True
+
         fcntl.lockf(self.__pointer, fcntl.LOCK_EX) 
         self.__databaseLocked = True
         signal.alarm(self.__openTimeoutSeconds)
         return self
 
+    def __timeoutRaiser(self, *arguments):
+        if not self.__databaseLocked:
+            raise Timeout('Database could not locked.')
+
+        raise Timeout
+
     def read(self):
         self.__pointer.seek(0)
         value = self.__pointer.read().strip()
+
         if not value:
-            from datetime import datetime
             value = datetime.utcnow().isoformat()
             self.write(value)
+
         return value
 
     def write(self, value):
